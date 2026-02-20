c251bf0736aceb68acbf110d2fba4dc358402d53
Linta <lintalukose2000@gmail.com>
2026-02-19 19:41:07 -0800

    My First Commit

---
+++ DemoMven/src/main/java/packm1/FirstMaven.java
@@ -0,0 +1,22 @@
+package packm1;
+
+public class FirstMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		
+		int num=5;
+		int num2=6;
+		float num3=4.5f;
+	    float base=3.5f;
+	    float height=4.8f;
+	    float Area=(base*height)/2;
+		System.out.println("Sum of "+num+" and "+num2+"= " +(num+num2));
+		System.out.println("Sum of "+num2+" and "+num3+"= " +(num2+num3));
+		System.out.println("My name is Linta");
+		System.out.println("I am 25 years old");
+		System.out.println("Area of Triangle with base "+base+"cm and height "+height+"cm is "+ Area+"cm²");
+	}
+
+}
+
